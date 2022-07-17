975fb73ee1fd92bd0e13c5fdf1be860eee7f4970
Carla Ferreira <ana_carla_0202@yahoo.com.br>
2022-07-17 16:06:10 -0300

    Quadrilatero

--- src/Quadrilatero/Main.java
+++ src/Quadrilatero/Main.java
@@ -5,8 +5,16 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Exercício quadrilátero");
-        Quadrilatero.area(3);
-        Quadrilatero.area(5, 5);
-        Quadrilatero.area(7, 8, 9);
+
+        double areaQuadrado = Quadrilatero.area(3);
+        System.out.println("Área do quadrado: " + areaQuadrado);
+
+        double areaRetangulo = Quadrilatero.area(5, 5);
+        System.out.println("Área do retângulo: " + areaRetangulo);
+
+        double areaTrapezio = Quadrilatero.area(7, 8, 9);
+        System.out.println("Área do trapézio: " + areaTrapezio);
+
+
     }
 }
